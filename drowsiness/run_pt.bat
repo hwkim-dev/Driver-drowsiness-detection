3004652ce89f1aab654dba9155de78b33094e86b
hwkim-dev <k1h6w4@gmail.com>
2024-06-12 13:21:36 +0900

    remove batch file
    remove batch file

--- drowsiness/run_pt.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd %~dp0
-echo C:\D\github\Driver-drowsiness-detection\drowsiness: %cd%
-C:\Users\hwKIM\anaconda3\python.exe "C:\D\github\Driver-drowsiness-detection\drowsiness\main.py"
-exit
